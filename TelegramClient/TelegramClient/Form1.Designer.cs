67bec64bbe7973c958dca02a95f29d308fe5a961
Aliaksandr_Harbunou <Aliaksandr_Harbunou@epam.com>
2016-12-01 18:40:06 +0300

    working send message (alfa) added authentificate, sign in, get recipient from account, send message

--- TelegramClient/TelegramClient/Form1.Designer.cs
+++ TelegramClient/TelegramClient/Form1.Designer.cs
@@ -34,13 +34,20 @@
             this.textBoxCode = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBoxPhoneTo = new System.Windows.Forms.TextBox();
+            this.recipientPhone = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.buttonAuth = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox_userlist = new System.Windows.Forms.ComboBox();
+            this.recipientName = new System.Windows.Forms.TextBox();
+            this.buttonSignIn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button_send
             // 
-            this.button_send.Location = new System.Drawing.Point(293, 226);
+            this.button_send.Enabled = false;
+            this.button_send.Location = new System.Drawing.Point(293, 145);
             this.button_send.Name = "button_send";
             this.button_send.Size = new System.Drawing.Size(126, 23);
             this.button_send.TabIndex = 0;
@@ -59,72 +66,130 @@
             // 
             // textBoxPhone
             // 
-            this.textBoxPhone.Location = new System.Drawing.Point(293, 39);
+            this.textBoxPhone.Location = new System.Drawing.Point(3, 35);
             this.textBoxPhone.Name = "textBoxPhone";
-            this.textBoxPhone.Size = new System.Drawing.Size(126, 20);
+            this.textBoxPhone.Size = new System.Drawing.Size(101, 20);
             this.textBoxPhone.TabIndex = 2;
+            this.textBoxPhone.Text = "375296852724";
             this.textBoxPhone.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPhone_KeyPress);
             // 
             // textBoxCode
             // 
-            this.textBoxCode.Location = new System.Drawing.Point(293, 89);
+            this.textBoxCode.Location = new System.Drawing.Point(3, 85);
             this.textBoxCode.Name = "textBoxCode";
             this.textBoxCode.Size = new System.Drawing.Size(100, 20);
             this.textBoxCode.TabIndex = 3;
             this.textBoxCode.Tag = "code";
+            this.textBoxCode.TextChanged += new System.EventHandler(this.textBoxCode_TextChanged);
             this.textBoxCode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxCode_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(293, 70);
+            this.label1.Location = new System.Drawing.Point(3, 66);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.Size = new System.Drawing.Size(59, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "code";
+            this.label1.Text = "Enter code";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(296, 20);
+            this.label2.Location = new System.Drawing.Point(6, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "phone";
+            this.label2.Text = "your phone number";
             // 
-            // textBoxPhoneTo
+            // recipientPhone
             // 
-            this.textBoxPhoneTo.Location = new System.Drawing.Point(534, 228);
-            this.textBoxPhoneTo.Name = "textBoxPhoneTo";
-            this.textBoxPhoneTo.Size = new System.Drawing.Size(100, 20);
-            this.textBoxPhoneTo.TabIndex = 6;
-            this.textBoxPhoneTo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.recipientPhone.Enabled = false;
+            this.recipientPhone.Location = new System.Drawing.Point(498, 229);
+            this.recipientPhone.Name = "recipientPhone";
+            this.recipientPhone.Size = new System.Drawing.Size(121, 20);
+            this.recipientPhone.TabIndex = 6;
+            this.recipientPhone.TextChanged += new System.EventHandler(this.textBoxPhoneTo_TextChanged);
+            this.recipientPhone.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPhoneTo_KeyPress);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(495, 228);
+            this.label3.Location = new System.Drawing.Point(457, 151);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(16, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "to";
+            // 
+            // buttonAuth
+            // 
+            this.buttonAuth.Location = new System.Drawing.Point(110, 32);
+            this.buttonAuth.Name = "buttonAuth";
+            this.buttonAuth.Size = new System.Drawing.Size(83, 23);
+            this.buttonAuth.TabIndex = 8;
+            this.buttonAuth.Text = "Authentificate";
+            this.buttonAuth.UseVisualStyleBackColor = true;
+            this.buttonAuth.Click += new System.EventHandler(this.buttonAuth_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonSignIn);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.buttonAuth);
+            this.groupBox1.Controls.Add(this.textBoxPhone);
+            this.groupBox1.Controls.Add(this.textBoxCode);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(293, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 113);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Authentificate";
+            // 
+            // comboBox_userlist
+            // 
+            this.comboBox_userlist.FormattingEnabled = true;
+            this.comboBox_userlist.Location = new System.Drawing.Point(498, 143);
+            this.comboBox_userlist.Name = "comboBox_userlist";
+            this.comboBox_userlist.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_userlist.TabIndex = 10;
+            this.comboBox_userlist.SelectedIndexChanged += new System.EventHandler(this.comboBox_userlist_SelectedIndexChanged);
+            // 
+            // recipientName
+            // 
+            this.recipientName.Enabled = false;
+            this.recipientName.Location = new System.Drawing.Point(498, 203);
+            this.recipientName.Name = "recipientName";
+            this.recipientName.Size = new System.Drawing.Size(121, 20);
+            this.recipientName.TabIndex = 12;
+            // 
+            // buttonSignIn
+            // 
+            this.buttonSignIn.Enabled = false;
+            this.buttonSignIn.Location = new System.Drawing.Point(110, 81);
+            this.buttonSignIn.Name = "buttonSignIn";
+            this.buttonSignIn.Size = new System.Drawing.Size(83, 23);
+            this.buttonSignIn.TabIndex = 9;
+            this.buttonSignIn.Text = "Sign In";
+            this.buttonSignIn.UseVisualStyleBackColor = true;
+            this.buttonSignIn.Click += new System.EventHandler(this.buttonSignIn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 261);
+            this.Controls.Add(this.recipientName);
+            this.Controls.Add(this.comboBox_userlist);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBoxPhoneTo);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBoxCode);
-            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.recipientPhone);
             this.Controls.Add(this.textBox_message);
             this.Controls.Add(this.button_send);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Telegram Client";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,8 +203,13 @@
         private System.Windows.Forms.TextBox textBoxCode;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBoxPhoneTo;
+        private System.Windows.Forms.TextBox recipientPhone;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonAuth;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox_userlist;
+        private System.Windows.Forms.TextBox recipientName;
+        private System.Windows.Forms.Button buttonSignIn;
     }
 }
 
--- TelegramClient/TelegramClient/Form1.cs
+++ TelegramClient/TelegramClient/Form1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using TeleSharp.TL;
 using TLSharp.Core;
@@ -10,7 +12,12 @@ namespace Telegram_Client
     public partial class Form1 : Form
     {
         string apiHash = "<Your API HASH>";
-        int apiId = 1;
+        int apiId = 81381;
+
+        private TLUser user = null;
+        bool resultConnect = false;
+        TelegramClient client = null;
+        List<TLUser> recipientUserlist = null;
 
         public Form1()
         {
@@ -18,7 +25,11 @@ namespace Telegram_Client
             InitClient();
         }
 
-        string PhoneNumber {
+        #region Property
+        string SessionHash { get; set; }
+                
+        string MyPhoneNumber
+        {
             get
             {
                 return textBoxPhone.Text;
@@ -29,7 +40,9 @@ namespace Telegram_Client
             }
         }
 
-        // код который придет от Telegram 
+        int RecipientID { get; set; }       
+
+        // code sms drom telegram
         string Code
         {
             get
@@ -54,84 +67,245 @@ namespace Telegram_Client
             }
         }
 
-        string recipientPhone
+        string RecipientPhone
         {
             get
             {
-                return textBoxPhoneTo.Text;
+                return recipientPhone.Text;
             }
             set
             {
-                textBoxPhoneTo.Text = value;
+                recipientPhone.Text = value;
             }
         }
 
-        bool resultConnect = false;
-        TelegramClient client;       
+        #endregion
 
-        private async void InitClient()
-        {                 
-            client = new TelegramClient(apiId, apiHash);
-            resultConnect = await client.ConnectAsync(true);
+        #region control Events
+
+        private void buttonAuth_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(MyPhoneNumber))
+            {
+                textBoxPhone.BackColor = Color.White;
+                RequestCode();
+            }
+            else
+            {
+                MessageBox.Show("Enter you Phone!");
+                textBoxPhone.Focus();
+                textBoxPhone.BackColor = Color.Red;
+            }
+        }
+
+        private void buttonSignIn_Click(object sender, EventArgs e)
+        {
+            SignIn();
         }
 
         private void button_send_Click(object sender, EventArgs e)
         {
-            if (String.IsNullOrEmpty(textBoxCode.Text))
+            if (string.IsNullOrEmpty(Code) || string.IsNullOrEmpty(SessionHash))
             {
                 RequestCode();
             }
             else
             {
-                SendMessageByPhone();
+                if (string.IsNullOrEmpty(RecipientPhone))
+                {
+                    MessageBox.Show("Choice recipient by phone or Name");             
+                    recipientPhone.BackColor = Color.Red;
+                }
+                else
+                {
+                    SendMessageByPhone();
+                }
             }
         }
 
-        private async void SendMessageByPhone()
-        {
-            //get available contacts
-            var result = await client.GetContactsAsync();
-
-            //find recipient in contacts
-            var user = result.users.lists
-                .Where(x => x.GetType() == typeof(TLUser))
-                .Cast<TLUser>()
-                .FirstOrDefault(x => x.phone == recipientPhone);
-
-            //send message
-            await client.SendMessageAsync(new TLInputPeerUser() { user_id = user.id }, Message);
-        }
-
+        //fix textBox text only digit
         private void textBoxPhone_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
-        }      
+        }
 
         private void textBoxCode_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
         }
 
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        private void textBoxPhoneTo_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
         }
 
+        private void textBoxCode_TextChanged(object sender, EventArgs e)
+        {
+            buttonSignIn.Enabled = !string.IsNullOrEmpty(Code) && !string.IsNullOrEmpty(MyPhoneNumber);
+            textBoxCode.BackColor = (string.IsNullOrEmpty(Code)) ? Color.Red : Color.White;            
+        }
+       
+        private void textBoxPhoneTo_TextChanged(object sender, EventArgs e)
+        {
+            recipientPhone.BackColor = (string.IsNullOrEmpty(recipientPhone.Text)) ? Color.Red : Color.White;
+        }
+
+        private void comboBox_userlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox_userlist.SelectedValue == null)
+            {
+                return;
+            }
+            // получаем весь выделенный объект
+            Recipient recipient = (Recipient)comboBox_userlist.SelectedItem;
+            recipientName.Text = string.Format("{0} {1}", recipient.FirstName, recipient.LastName) ;
+            RecipientPhone = recipient.Phone;
+            RecipientID = recipient.Id;
+        }
+
+        #endregion
+        
+        private async void InitClient()
+        {                 
+            client = new TelegramClient(apiId, apiHash);
+            resultConnect = await client.ConnectAsync(true);
+        }
+            
+        private async void SendMessageByPhone()
+        {
+            if (string.IsNullOrEmpty(Message))
+            {
+                MessageBox.Show("Enter your message");
+                textBox_message.Focus();
+                return;
+            }
+            if (RecipientID == 0)
+            {                
+                MessageBox.Show("Choice recipient by phone or Name");
+                recipientPhone.Focus();
+                recipientPhone.BackColor = Color.Red;
+                return;
+            }
+
+            if (client != null)
+            {
+                try
+                {
+                    if (user == null)
+                    {
+                        //var hash = await client.SendCodeRequestAsync(PhoneNumber); //отсылаем запрос на создании сессии 
+                        user = await client.MakeAuthAsync(MyPhoneNumber, SessionHash, Code);
+                    }
+                    if (user == null)
+                    {
+                        Thread.Sleep(1000);
+                        if (user == null)
+                        {
+                            MessageBox.Show("user == null");
+                            return;
+                        }
+                    }               
+                    //send message
+                    await client.SendMessageAsync(new TLInputPeerUser() { user_id = RecipientID }, Message);
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error! " + ex.Message);
+                }
+            }          
+        }
+
+        private async Task FillRecipienstList()
+        {
+            recipientUserlist = null;
+            if (client != null)
+            {
+                try
+                {
+                    if (user == null)
+                    {
+                        //var hash = await client.SendCodeRequestAsync(PhoneNumber); //отсылаем запрос на создании сессии 
+                        user = await client.MakeAuthAsync(MyPhoneNumber, SessionHash, Code);
+                    }
+                    if (user == null)
+                    {
+                        Thread.Sleep(1000);
+                        if (user == null)
+                        {
+                            MessageBox.Show("user2 == null");
+                            return;
+                        }
+                    }
+                    //get available contacts
+                    var result = await client.GetContactsAsync();
+
+                    if (result != null)
+                    {
+                        TLVector<TLAbsUser> userS = result.users;
+                        List<TLAbsUser> usersList = userS.lists;
+                        recipientUserlist = new List<TLUser>();
+                        List<Recipient> recipientList = new List<Recipient>();
+                        foreach (TLUser userTLU in usersList)
+                        {
+                            TLUser u = userTLU as TLUser;
+                            if (u != null)
+                            {                               
+                                recipientUserlist.Add(u);
+                                recipientList.Add(new Recipient {
+                                    Id = u.id,
+                                    Name = string.Format("{0} {1} ({2})", u.first_name, u.last_name, u.phone),
+                                    Phone = u.phone, FirstName = u.first_name, LastName = u.last_name});
+                            }
+                        }
+
+                        comboBox_userlist.DataSource = recipientList;
+                        comboBox_userlist.DisplayMember = "Name";
+                        comboBox_userlist.ValueMember = "Id";                      
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error! " + ex.Message);
+                }
+            }
+        }
+
         private async void RequestCode()
         {
+            Code = string.Empty;       
             if (resultConnect)
             {   //textBox_message.Text
-                var hash = await client.SendCodeRequestAsync(PhoneNumber); //отсылаем запрос на создании сессии 
-                while (string.IsNullOrEmpty(Code))
+                try
                 {
-                    Thread.Sleep(500);
+                    SessionHash = await client.SendCodeRequestAsync(MyPhoneNumber); //отсылаем запрос на создании сессии  
+                    MessageBox.Show("Enter Code from SMS");                    
+                    textBoxCode.BackColor = Color.Red;
                 }
-                var user = await client.MakeAuthAsync(PhoneNumber, hash, Code); // создаем сессию
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error " + ex.Message);
+                }
             }
             else
             {
                 MessageBox.Show("Not connected");
             }
         }
+
+        private async void SignIn()
+        {
+            await FillRecipienstList();
+            button_send.Enabled = (RecipientID != 0) && !string.IsNullOrEmpty(Code) && !string.IsNullOrEmpty(MyPhoneNumber) && !string.IsNullOrEmpty(SessionHash);            
+        }     
+    }
+
+    class Recipient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
     }
 }
